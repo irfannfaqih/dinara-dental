02e5ea2c52a9c8e7c11af19ed426cd6da2960973
faqih <muhamadirfanfaqih9a@gmail.com>
2026-05-07 16:47:54 +0800

    Revert navbar logo and keep browser tab icon

--- src/components/Global/Navbar.tsx
+++ src/components/Global/Navbar.tsx
@@ -35,9 +35,9 @@ export default function Navbar() {
           <div className="w-[110px] sm:w-[200px] flex items-center justify-start">
             <a href="#" className="flex items-center group">
               <Image
-                src="/logo-browser.png"
+                src="/logo.png"
                 alt="Dinara Dental Care"
-                width={40}
+                width={120}
                 height={40}
                 className="h-8 sm:h-10 w-auto object-contain mix-blend-multiply transition-transform duration-300 group-hover:scale-105"
               />
